fc240012d85d7b5a09cda1d6686cce8b0650f1be
tiagomartinho <tiago.martinho@xpeppers.com>
2016-11-18 14:11:09 +0100

    simplifies notifies view test

--- app/src/test/java/com/elit/trackerbot/TrackingPresenterTest.java
+++ app/src/test/java/com/elit/trackerbot/TrackingPresenterTest.java
@@ -3,12 +3,8 @@ package com.elit.trackerbot;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -31,18 +27,11 @@ public class TrackingPresenterTest {
 
     @Test public void
     notifies_view_when_track_is_completed() throws Exception {
-        final String information = "Did track project XXX";
+        final String anInformation = "Did track project XXX";
         final TrackingPresenter presenter = new TrackingPresenter(view, service);
-        doAnswer(new Answer<String>() {
-            @Override
-            public String answer(InvocationOnMock invocation) throws Throwable {
-                presenter.didTrack(information);
-                return "";
-            }
-        }).when(service).track(presenter);
 
-        presenter.track();
+        presenter.didTrack(anInformation);
 
-        verify(view).showTrackInformation(information);
+        verify(view).showTrackInformation(anInformation);
     }
 }
